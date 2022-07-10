d046d957de0e6e45187cfc6d44f872e788d8b149
Елена <el_el_@mail.ru>
2022-07-10 09:54:55 +0300

    2

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,18 @@
-document.querySelector('.main-nav__toggle')
+// header
+
+document.querySelector('.main-nav__toggle-wrapper')
   .addEventListener('click', function(){
   document.querySelector('.main-nav__toggle').classList.toggle('main-nav__toggle--active')
 })
+
+
+// map
+const map = L.map('map').setView([59.968137, 30.316272], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
+const greenIcon = L.icon({
+  iconUrl: '../img/icon/footer/logo-map.svg',
+  iconSize:     [38, 50],
+});
+L.marker([59.968137, 30.316272], {icon: greenIcon}).addTo(map);
